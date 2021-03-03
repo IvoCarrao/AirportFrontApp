ff518a4d8f4b25b1a2e9e3b933e2bd2596b6c490
Ivo Carrão <ivocarrao@hotmail.com>
2021-03-03 16:22:10 +0000

    added webClient builder as a bean

--- src/main/java/org/AirportFrontApp/StartAirportFrontApplication.java
+++ src/main/java/org/AirportFrontApp/StartAirportFrontApplication.java
@@ -2,9 +2,17 @@ package org.AirportFrontApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class StartAirportFrontApplication {
+
+    @Bean
+    public WebClient.Builder getWebClientBuilder(){
+        return WebClient.builder();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(org.AirportFrontApp.StartAirportFrontApplication.class, args);
     }
